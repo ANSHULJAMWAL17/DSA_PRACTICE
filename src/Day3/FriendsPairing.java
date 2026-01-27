59e721936cb2d8d192da03ed8fa8e7596c72438c
AnshulJamwal17 <anshuljamwal90@gmail.com>
2026-01-27 20:32:25 +0530

    Day3

--- src/Day3/FriendsPairing.java
+++ src/Day3/FriendsPairing.java
@@ -1,4 +1,18 @@
 package Day3;
 
+import java.util.Arrays;
+
 public class FriendsPairing {
+    public long helper(int n,long dp[]){
+        if(n<=1) return 1;
+        if(n==2) return 2;
+        if(dp[n]!=-1) return dp[n];
+        dp[n]=helper(n-1,dp)+(n-1)*helper(n-2,dp);
+        return dp[n];
+    }
+    public long countFriendsPairings(int n) {
+        long dp[]=new long[n+1];
+        Arrays.fill(dp,-1);
+        return helper(n,dp);
+    }
 }
